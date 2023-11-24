935c247afd936052991147a5d0d7bc0eee09a81f
minseoya <119482288+minseoya@users.noreply.github.com>
2023-11-24 14:55:56 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,15 @@
+function solution(progresses, speeds) {
+    let days = progresses.map((num,i)=>Math.ceil((100-num)/speeds[i]));
+   
+    let answer=[0];
+    let maxDay=days[0];
+      for(let i = 0, j = 0; i< days.length; i++){
+        if(days[i] <= maxDay) {
+            answer[j] += 1;
+        } else {
+            maxDay = days[i];
+            answer[++j] = 1;
+        }
+    }
+    return answer;
+}
