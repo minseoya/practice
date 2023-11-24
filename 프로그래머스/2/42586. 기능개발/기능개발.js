a8277bbdf66e24df01a434d2816955952e5434c5
minseoya <119482288+minseoya@users.noreply.github.com>
2023-11-24 16:08:04 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,30 @@
-
 function solution(progresses, speeds) {
-    let days = progresses.map((num,i)=>Math.ceil((100-num)/speeds[i]));
-   
-    let answer=[0];
-    let maxDay=days[0];
-      for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
-        }
+    let answer = [];
+    let queue = [];
+
+    // 작업이 완료되는데 필요한 일수를 큐에 저장
+    for (let i = 0; i < progresses.length; i++) {
+        let remainDays = Math.ceil((100 - progresses[i]) / speeds[i]);
+        queue.push(remainDays);
     }
+
+    while (queue.length > 0) {
+        let deployDay = queue.shift(); // 가장 앞의 작업의 배포일
+        let count = 1; // 현재 배포에서의 작업 수
+
+        // 현재 작업보다 뒤에 있는 작업 중에서 배포 가능한 작업 수 계산
+        while (queue.length > 0 && deployDay >= queue[0]) {
+            queue.shift();
+            count++;
+        }
+
+        answer.push(count); // 배포 가능한 작업 수 저장
+    }
+
     return answer;
 }
+
+// Example
+const progresses = [93, 30, 55];
+const speeds = [1, 30, 5];
+console.log(solution(progresses, speeds)); // Output: [2, 1]
