895f528d9db064a4558fd12623e33f5cab8226c0
minseoya <119482288+minseoya@users.noreply.github.com>
2023-11-24 14:42:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.34 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{
+    var answer = [];
+    arr.forEach(num=>{
+        if(answer[answer.length-1]!==num){
+            answer.push(num)
+        }
+    })
+    return answer;
+}
