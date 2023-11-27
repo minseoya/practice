1095deed9aeea8c8c64412b05197a9fb16d98be9
minseoya <119482288+minseoya@users.noreply.github.com>
2023-11-27 11:17:33 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.74 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,11 @@
+function solution(A,B){
+   let answer = 0;
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b-a);
+
+    for(let i= 0;i<B.length;i++){
+        answer += A[i]*B[i]
+    }
+    
+    return answer;
+}
