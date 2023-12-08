45428ef08a88c4f7ef375f6caa7368710c116b80
minseoya <119482288+minseoya@users.noreply.github.com>
2023-12-08 17:22:25 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int i,answer = 0;
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            answer+=i;
+        }        
+    }
+   
+    return answer;
+}
